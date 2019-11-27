10e660756104faa2ac25242626000e1f801e8840
OrEiNor <57980662+OrEiNor@users.noreply.github.com>
2019-11-27 15:18:11 +0700

    Change name in test folder

--- tests/server.test.js
+++ tests/server.test.js
@@ -11,7 +11,7 @@ describe("GET /", () => {
             .get('/')
             .end((err, res) => {
                 res.should.have.status(200);
-                res.text.should.contain('Hello Intuorn!!!!!!!!!!');
+                res.text.should.contain('Hello Janettre!!!!!!!!!!');
                 done();
             });
     });
